c537aab8838d1fa1cc1b78080d59f50bd022f1a9
gigis-lab <churn82@gmail.com>
2023-10-13 00:09:07 +0900

    주석제거

--- app.py
+++ app.py
@@ -186,28 +186,7 @@ def write_order():
     return jsonify({'result': 'success'})
 
 
-# @app.route('/list', methods=['GET'])
-# def read_orders():
-#     current_date = str(datetime.now().date()).replace('-','')
-#     current_time = str(datetime.now().strftime("%H%M"))
-#     current_datetime = current_date + current_time
-    
-#     order_datetime = ''
-    
-#     valid_orders = []
 
-#     result = list(db.order.find({}, {'_id':0}))
-
-#     for order in result:
-#         order_date = order['order_date'].replace('-','')
-#         order_time = order['order_time'].replace(':','')
-#         order_datetime = order_date + order_time
-#         print('order_datetime : ', order_datetime)
-#         if (int(order_datetime) > int(current_datetime)):
-#             valid_orders.append(order)
-#             print('valid_orders', valid_orders)
-    
-#     return jsonify({'result': 'success', 'orders': valid_orders})
 
 
 if __name__ == '__main__':
