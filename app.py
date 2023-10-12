6461ee02d28018caa6908959ff4fd3300c88f89d
jinjung0101 <qwe5344641@gmail.com>
2023-10-12 19:14:17 +0900

    로그인 회원가입 추가

--- app.py
+++ app.py
@@ -12,52 +12,15 @@ from datetime import datetime
 
 
 # JWT 토큰을 만들 때 필요한 비밀문자열
-SECRET_KEY = 'MUKGOHAE'
+
 
 app = Flask(__name__)
-
+app.secret_key = 'MUKGOHAE'
 # MongoDB에 연결
 client = MongoClient('localhost', 27017)
 
 # 데이터베이스 생성
 db = client.dbmukgohae
-
-
-#####################################################################################
-# 이 부분은 코드를 건드리지 말고 그냥 두세요. 코드를 이해하지 못해도 상관없는 부분입니다.
-#
-# ObjectId 타입으로 되어있는 _id 필드는 Flask 의 jsonify 호출시 문제가 된다.
-# 이를 처리하기 위해서 기본 JsonEncoder 가 아닌 custom encoder 를 사용한다.
-# Custom encoder 는 다른 부분은 모두 기본 encoder 에 동작을 위임하고 ObjectId 타입만 직접 처리한다.
-# class CustomJSONEncoder(json.JSONEncoder):
-#     def default(self, o):
-#         if isinstance(o, ObjectId):
-#             return str(o)
-#         return json.JSONEncoder.default(self, o)
-
-
-# class CustomJSONProvider(JSONProvider):
-#     def dumps(self, obj, **kwargs):
-#         return json.dumps(obj, **kwargs, cls=CustomJSONEncoder)
-
-#     def loads(self, s, **kwargs):
-#         return json.loads(s, **kwargs)
-
-
-# 위에 정의되 custom encoder 를 사용하게끔 설정한다.
-# app.json = CustomJSONProvider(app)
-
-# 여기까지 이해 못해도 그냥 넘어갈 코드입니다.
-# #####################################################################################
-
-
-#####
-# 아래의 각각의 @app.route 은 RESTful API 하나에 대응됩니다.
-# @app.route() 의 첫번째 인자는 API 경로,
-# 생략 가능한 두번째 인자는 해당 경로에 적용 가능한 HTTP method 목록을 의미합니다.
-
-# API #1: HTML 틀(template) 전달
-#         틀 안에 데이터를 채워 넣어야 하는데 이는 아래 이어지는 /api/list 를 통해 이루어집니다.
 
 
 @app.route('/')
@@ -98,6 +61,9 @@ def api_register():
         return jsonify({'result': 'success'})
 
 
+
+
+
 # 로그인
 @app.route('/api/login', methods=['POST'])
 def api_login():
@@ -110,39 +76,30 @@ def api_login():
     # email, 암호화된pw을 가지고 해당 유저를 찾습니다.
     result = db.user.find_one({'email': email_receive, 'pw': pw_hash})
 
-    # 찾으면 JWT 토큰을 만들어 발급합니다.
     if result is not None:
-        # JWT 토큰 생성
-        payload = {
-            'email': email_receive,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=100)
-        }
-        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-
-        # token을 줍니다.
-        return jsonify({'result': 'success', 'token': token})
-    # 찾지 못하면
+        # 사용자가 인증되었을 때 세션 또는 쿠키를 설정
+        session['email'] = email_receive
+        return jsonify({'result': 'success', 'message': '로그인 성공'})
     else:
-        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+        return jsonify({'result': 'fail', 'message': '아이디 또는 비밀번호가 일치하지 않습니다.'})
+
+#로그아웃
+@app.route('/logout')
+def logout():
+    session.pop('email', None)  # 세션에서 사용자 정보 삭제
+    return redirect(url_for('login'))  # 로그인 페이지로 리디렉션
 
 
-# 보안: 로그인한 사용자만 통과할 수 있는 API
-@app.route('/api/isAuth', methods=['GET'])
-def api_valid():
-    token_receive = request.cookies.get('mytoken')
-    try:
-        # token을 시크릿키로 디코딩합니다.
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        # payload 안에 email가 들어있습니다. 이 email로 유저정보를 찾습니다.
-        userinfo = db.user.find_one({'email': payload['email']}, {'_email': 0})
-        return jsonify({'result': 'success', 'nickname': userinfo['nick']})
-    except jwt.ExpiredSignatureError:
-        # 위를 실행했는데 만료시간이 지났으면 에러가 납니다.
-        return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
-    except jwt.exceptions.DecodeError:
-        # 로그인 정보가 없으면 에러가 납니다!
-        return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
-    
+
+#인증
+@app.route('/api/secure', methods=['GET'])
+def secure_api():
+    if 'email' in session:
+        # 사용자가 로그인되어 있을 때만 접근 가능
+        return jsonify({'result': 'success', 'message': '인증된 사용자입니다.'})
+    else:
+        return jsonify({'result': 'fail', 'message': '인증이 필요합니다.'})
+
 
 
 
